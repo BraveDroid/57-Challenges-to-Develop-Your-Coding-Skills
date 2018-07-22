c86d27831ec50b3d4b6f8232cc01f2d2b560f204
refka-farid <refka.farid@gmail.com>
2018-07-23 00:07:26 +0200

    Ready for testing.

---
+++ 2. Input, Processing, and Output/java/Exercice 2/src/com/bravedroid/Main.java
@@ -0,0 +1,15 @@
+package com.bravedroid;
+
+import com.bravedroid.presenter.Presenter;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Presenter counterCharInString = new Presenter();
+        counterCharInString.promptStringAdedByUser();
+        counterCharInString.printNumberOfCharacter();
+           }
+    }
+
+
