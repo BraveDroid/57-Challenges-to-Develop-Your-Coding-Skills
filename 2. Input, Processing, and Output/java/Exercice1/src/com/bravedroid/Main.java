cbee08544f92c062122c2aad40321c012997fecd
refka-farid <refka.farid@gmail.com>
2018-07-25 21:09:51 +0200

    reformat code

--- 2. Input, Processing, and Output/java/Exercice1/src/com/bravedroid/Main.java
+++
@@ -1,8 +0,0 @@
-package com.bravedroid;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- 2. Input, Processing, and Output/java/Exercice1/src/com/bravedroid/passive_way/Main.java
+++ 2. Input, Processing, and Output/java/Exercice1/src/com/bravedroid/passive_way/Main.java
@@ -3,10 +3,7 @@ package com.bravedroid.passive_way;
 import com.bravedroid.passive_way.applicatiion.GreetingAdder;
 import com.bravedroid.passive_way.presentation.Presenter;
 
-import java.io.IOException;
-
 public class Main {
-
     public static void main(String[] args) throws Exception {
         GreetingAdder greetingAdder = new GreetingAdder();
         Presenter presenter = new Presenter(greetingAdder);
