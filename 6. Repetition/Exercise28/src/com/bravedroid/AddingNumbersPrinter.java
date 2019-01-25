a856f821304a562f503e03466baa0edc4dd79702
refka-farid <refka.farid@gmail.com>
2019-01-25 18:22:32 +0100

    add other variant of loops

--- 6. Repetition/Exercise28/src/com/bravedroid/AddingNumbersPrinter.java
+++ 6. Repetition/Exercise28/src/com/bravedroid/AddingNumbersPrinter.java
@@ -6,18 +6,61 @@ import java.io.InputStreamReader;
 
 class AddingNumbersPrinter {
   private BufferedReader input;
+  private int sum;
+  private String inputFromUser;
 
   AddingNumbersPrinter() {
     input = new BufferedReader(new InputStreamReader(System.in));
   }
-
+//variant with do{} while()
   void readNumber() throws IOException {
     int sum = 0;
     int i = 0;
-    while (i < 5) {
-      System.out.println("i: "+i);
+    do {
+      System.out.println("i: " + i);
       System.out.println("Enter A Number : ");
-      String inputFromUser = input.readLine();
+      inputFromUser = input.readLine();
+      if (mustExit(inputFromUser)) {
+        return;
+      }
+      if (isValidInput(inputFromUser)) {
+        int numberFromUser = Integer.parseInt(inputFromUser);
+        sum += numberFromUser;
+        i++;
+      }
+    } while (i < 5);
+    System.out.println("The Total is " + sum);
+  }
+  //variant with for()
+  void readNumber1() throws IOException {
+    for (int i = 0; i < 5; i = incrementValue(i)) {
+      System.out.println("i: " + i);
+      System.out.println("Enter A Number : ");
+      inputFromUser = input.readLine();
+      if (mustExit(inputFromUser)) {
+        return;
+      }
+    }
+    System.out.println("The Total is " + sum);
+
+  }
+
+  private int incrementValue(int value) {
+    if (isValidInput(inputFromUser)) {
+      int numberFromUser = Integer.parseInt(inputFromUser);
+      sum += numberFromUser;
+      value++;
+    }
+    return value;
+  }
+  //variant with while()
+  void readNumber2() throws IOException {
+    int sum = 0;
+    int i = 0;
+    while (i < 5) {
+      System.out.println("i: " + i);
+      System.out.println("Enter A Number : ");
+      inputFromUser = input.readLine();
       if (mustExit(inputFromUser)) {
         return;
       }
