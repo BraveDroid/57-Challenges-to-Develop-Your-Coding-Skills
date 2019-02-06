8ddde44ffcf39d7e8434cfbb19277feb47421ce4
refka-farid <refka.farid@gmail.com>
2019-02-06 22:56:52 +0100

    add solution for exercise 35

---
+++ 6. Repetition/java/Exercise35/src/main/java/com/bravedroid/util/Logger.java
@@ -0,0 +1,15 @@
+package com.bravedroid.util;
+
+public class Logger {
+  private static boolean isDebug;
+
+  public Logger(boolean isDebug) {
+    Logger.isDebug = isDebug;
+  }
+
+  public void log(Object msg) {
+    if (isDebug) {
+      System.out.println("*******"+ msg+"*******");
+    }
+  }
+}
