244982a938520e4a2affbd28b43d12438349088c
refka-farid <refka.farid@gmail.com>
2019-02-13 19:43:21 +0100

    remove redunded project

--- 7. Data Structures/Exercise38/src/main/java/com/bravrdroid/domain/EvenNumberFilter.java
+++
@@ -1,9 +0,0 @@
-package com.bravrdroid.domain;
-
-import java.util.Arrays;
-
-public class EvenNumberFilter {
-  public int[] filterEvenNumbers(int[] inputNumbers) {
-    return Arrays.stream(inputNumbers).filter(value -> value % 2 == 0).toArray();
-  }
-}
